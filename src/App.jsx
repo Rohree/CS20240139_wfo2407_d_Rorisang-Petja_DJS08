e89c1ed59df5769d5d9a07ad8643a7bea383b616
rohree <hellororyy880@gmail.com>
2024-11-18 11:33:49 +0200

    Updated DJS08

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from "./pages/Vans"
+import VansDetail from "./pages/VansDetail"
+import "./server"
+
 
 function App() {
   return (
@@ -9,12 +13,15 @@ function App() {
       <header>
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
+          <Link to="/Vans">Vans</Link>
           <Link to="/about">About</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VansDetail />} />
       </Routes>
     </BrowserRouter>
   )
